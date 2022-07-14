fbe1ac957d3e17ac541e3e50d67a561669e0087c
Dwiksu <dwiik.kusuma@gmail.com>
2022-07-14 15:22:52 +0800

    membuat fungsi form agar bisa mengirim data ke spreadsheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,15 @@ window.addEventListener("scroll", () => {
       navbar.classList.remove("shadow-sm");
    }
 });
+
+
+// FORM KE SPREADSHEET
+const scriptURL = 'https://script.google.com/macros/s/AKfycbytyFvLUlzTi9A0Zt21SN8p_FxMO2rLdk37-xqJjNx2eelu8UXTqdaZmsm4ZmAma2KC/exec'
+const form = document.forms['Message']
+      
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+   .then(response => alert('Success!'))
+   .catch(error => console.error('Error!', error.message))
+})
