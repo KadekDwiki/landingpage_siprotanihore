44b7815ae50abf8c4e2c7eab980abc127694d6d7
Dwiki Nusanjaya <nusanjayadwiki@gmail.com>
2022-07-15 13:32:55 +0800

    memperbaiki navbar dan caraousel, membuat section about us, mencoba membuat animasi loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,16 @@ window.addEventListener("scroll", () => {
    if (window.scrollY >= 50) {
       navbar.classList.add("bg-white");
       navbar.classList.add("shadow-sm");
+      navbar.classList.remove("py-3");
    } else {
       navbar.classList.remove("bg-white");
       navbar.classList.remove("shadow-sm");
+      navbar.classList.add("py-3");
    }
+});
+
+// loader
+const loader = document.querySelector(".loader-wrapper");
+window.addEventListener("load", () => {
+   loader.classList.add("loader");
 });
